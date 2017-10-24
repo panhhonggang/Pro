ab56ed8c19c167469d8c4736ac7f393cdcdc908a
chenchangping <chenchangping@foxmail.com>
2017-10-24 09:49:02 +0800

    2017-10-24 09:48 添加设备两个页面 需要王工做页面处理

--- project/Application/Admin/Controller/DevicesController.class.php
+++ project/Application/Admin/Controller/DevicesController.class.php
@@ -10,23 +10,37 @@ use Think\Controller;
  */
 class DevicesController extends Controller
 {
+    /**
+     * 显示设备列表
+     */
     public function devicesList()
     {
         $device = D('Devices');
+        $deviceList = "Hello";
+        $this->assign('deviceList', $deviceList);
         $this->display('devicesList');
     }
 
+    /**
+     * 显示设备添加页面
+     */
     public function show_add_device()
     {
         $this->display('show_add_device');
     }
 
+    /**
+     * 设备添加处理
+     */
     public function add_device()
     {
-        $bool = D('Devices');
-        $code = I(post.code_id);
-        if( empty($code) || strlen($code) != 16 ){
+        $devices = D('Devices');
+        $code = I(get.code_id);
+        /*if( empty($code) || strlen($code) != 16 ){
             $this->error('设备码输入错误', 'devicesList', 0);
+        }*/
+        if( !$devices->create( $code )){
+            exit( $devices->getError() );
         }
 
     }
--- project/Application/Admin/Model/DevicesModel.class.php
+++ project/Application/Admin/Model/DevicesModel.class.php
@@ -4,8 +4,12 @@ use Think\Model;
 
 class DevicesModel extends Model
 {
+    protected $_validate = array(
+        array('code_id', length,'设备码不正确', 16),
+    );
+
     public function getDevicesInfo()
     {
-        $this->where()
+        $this->where()->select();
     }
 }
